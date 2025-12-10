c27bd70e41712c0e0151cfe97eb1f7ae1bb6e32a
İdil Gürsoy <64634611+idilsg@users.noreply.github.com>
2025-12-10 17:05:09 +0300

    primary pages added
    created feed, login, profile, new post pages + did some changes according to them in main

---
+++ lib/pages/new_post_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class NewPostPage extends StatelessWidget {
+  const NewPostPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
